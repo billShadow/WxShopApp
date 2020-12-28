cede772728ba21d80cdbca044d6ec671637318da
bill <bill_xiaobin@163.com>
2020-12-29 00:00:56 +0800

    商品详情界面逻辑

--- pages/goos_detail/index.js
+++ pages/goos_detail/index.js
@@ -1,66 +1,36 @@
 // pages/goos_detail/index.js
+import {request} from "../../request/index.js";
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    swip_imgs:[],
+    goods_info:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    const {goods_id} = options;
+    this.getGoodsDetail(goods_id);
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  getGoodsDetail(goods_id) {
+    request({url:"goods/detail?goods_id="+goods_id})
+    .then(res=>{
+      console.log(res);
+      const g_info = res.data.message;
+        this.setData({
+          goods_info:{
+            goods_name:g_info.goods_name,
+            goods_price:g_info.goods_price,
+            goods_introduce:g_info.goods_introduce
+          },
+          swip_imgs:res.data.message.pics
+        });
+    });
   }
 })
