eb38dcf65d634c5a526107efcbc8044927658081
bill <bill_xiaobin@163.com>
2020-12-29 23:02:13 +0800

    详情界面完善

--- pages/goos_detail/index.js
+++ pages/goos_detail/index.js
@@ -10,6 +10,7 @@ Page({
     swip_imgs:[],
     goods_info:[]
   },
+  goodsInfo:{},
 
   /**
    * 生命周期函数--监听页面加载
@@ -23,6 +24,7 @@ Page({
     .then(res=>{
       console.log(res);
       const g_info = res.data.message;
+      this.goodsInfo = g_info;
         this.setData({
           goods_info:{
             goods_name:g_info.goods_name,
@@ -32,5 +34,24 @@ Page({
           swip_imgs:res.data.message.pics
         });
     });
+  },
+  handlePrevewImage(e){
+    const current_img = e.currentTarget.dataset.url;
+    const urls = this.goodsInfo.pics.map(v=>v.pics_mid);
+    wx.previewImage({
+      current: current_img, // 当前显示图片的http链接
+      urls: urls // 需要预览的图片http链接列表
+    })
+  },
+  handleAddCar(){
+    let cart = wx.getStorageSync('cart') || [];
+    let index = cart.findIndex(v=>v.goods_id === this.goodsInfo.goods_id);
+    if (index === -1) {
+        this.goodsInfo.num = 1;
+        cart.push(this.goodsInfo)
+    } else {
+        cart[index].num++;
+    }
+    wx.setStorageSync('cart', cart);
   }
 })
